7fb69f7152d2267159e4a1fd9ae0ae68732b3033
hydroperx <hydroperfox@gmail.com>
2025-11-10 08:05:40 -0300

    upd

---
+++ src/liveTiles/CoreTiles.ts
@@ -0,0 +1,40 @@
+// third-party
+import assert from "assert";
+import { TypedEventTarget } from "@hydroperx/event";
+
+// local
+import { REMObserver } from "../utils/REMObserver";
+import * as MathUtils from "../utils/MathUtils";
+
+/**
+ * Live tiles core implementation.
+ */
+export class CoreTiles extends (EventTarget as TypedEventTarget<CoreTilesEventMap>) {
+  /**
+   * Constructs a `CoreTiles` instance.
+   */
+  public constructor(params: {
+    classNames: CoreTilesClassNames,
+  }) {
+    super();
+  }
+}
+
+/**
+ * Class names used by `CoreTiles`.
+ */
+export type CoreTilesClassNames = {
+  group: string,
+  groupLabel: string,
+  groupLabelText: string,
+  groupTiles: string,
+  tile: string,
+  tileDND: string,
+};
+
+/**
+ * Events emitted by `Tiles` instances.
+ */
+export type CoreTilesEventMap = {
+  //
+};
